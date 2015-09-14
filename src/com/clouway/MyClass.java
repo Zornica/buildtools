7223155600bc1a27272d6d1080f7e8a0f22f01bf
Zornica <zornicaewgeniewa@gmail.com>
2015-09-14 13:34:33 +0300

    Initial commit

---
+++ src/com/clouway/MyClass.java
@@ -0,0 +1,19 @@
+package com.clouway;
+
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * Created by Zornitsa Petkova on 9/10/15.
+ */
+public class MyClass {
+
+  private static final Logger logger = Logger.getLogger(MyClass.class);
+
+  public static void main(String[] args) {
+   logger.debug("Hello World");
+  }
+
+
+}
